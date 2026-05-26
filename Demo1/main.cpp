b802413a98b21e2b37d269a58764e0897d8ec972
hongkai <hongkai2938870070@outlook.com>
2026-05-26 08:26:47 +0800

    添加项目文件。

---
+++ Demo1/main.cpp
@@ -0,0 +1,8 @@
+#include "test.h"
+
+int main()
+{
+
+
+	return 0;
+}
---
+++ Demo1/test.h
@@ -0,0 +1,49 @@
+#ifndef _TEST_H_
+#define _TEST_H_
+
+#include<iostream>
+using namespace std;
+
+class Test
+{
+public:
+	Test();
+
+	~Test();
+private:
+	int m_iVal;
+};
+
+class TestA
+{
+public:
+	TestA();
+
+	~TestA();
+private:
+	int m_iVal;
+};
+
+
+class TestB
+{
+public:
+	TestB();
+
+	~TestB();
+private:
+	int m_iVal;
+};
+
+
+class TestC
+{
+public:
+	TestC();
+
+	~TestC();
+private:
+	int m_iVal;
+};
+
+#endif
